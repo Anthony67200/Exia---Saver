f13ad2b148c817a84b1a1ae0c44758372106edbf
Loulouv <louis.vernet@viacesi.fr>
2016-12-15 10:43:42 +0100

    Update launcher.h

--- launcher.h
+++ launcher.h
@@ -1,1 +1,6 @@
-void sauvegarde(int fichier, int image);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> // Bibliothèque pour le Rand
+#include <sys/stat.h>;
+
+void sauvegarde(int, int);
